8abce33b79ac979e3f30919f5916c59a1dc61c38
Nikita Kononenko <jadnostt@gmail.com>
2024-08-06 20:17:32 -0700

    feat: skeleton loading

--- components/WorksSection.tsx
+++ components/WorksSection.tsx
@@ -1,12 +1,12 @@
 "use client"
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import works from '../lib/works.json';
 import Image from 'next/image'
 
 const WorksSection: React.FC = () => {
   const [gap, setGap] = useState<string>('0');
+  const [loadedImages, setLoadedImages] = useState<Record<number, boolean>>({});
 
-  // Группируем работы по значению row
   const groupedWorks = works.reduce((acc, work) => {
     const row = work.row;
     if (!acc[row]) {
@@ -29,6 +29,15 @@ const WorksSection: React.FC = () => {
     setGap(newGap);
   }, [groupedWorks]);
 
+  const handleImageLoad = (index: number) => {
+    //console.log(`Image ${index} loaded`);
+    setLoadedImages(prevState => ({ ...prevState, [index]: true }));
+  };
+
+  useEffect(() => {
+    //console.log('Loaded images state updated:', loadedImages);
+  }, [loadedImages]);
+
   return (
     <section id='works'>
       {Object.entries(groupedWorks).map(([row, worksInRow]) => (
@@ -44,17 +53,25 @@ const WorksSection: React.FC = () => {
           >
             {worksInRow.map((work, index) => (
               <div key={index} className="bento">
-                <Image
-                    src={`/works/${work.image}`}
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    style={{ width: '100%', height: 'auto'}}
-                    alt={`${work.title}`}
-                />
-                <div className="flex justify-between">
-                    <p>{work.title}</p>
-                    <p className='opacity-50'>{work.customer}</p>
+                <div style={{ position: 'relative', width: '100%', height: '290px' }}>
+                    {!loadedImages[index] && (
+                      <div className='animate-pulse bg-kononenko border-kononenko' style={{ width: '100%', height: '100%', borderRadius: '10px', position: 'absolute', top: 0, left: 0}} />
+                    )}
+                    <Image
+                      src={`/works/${work.image}`}
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      style={{ width: '100%', height: 'auto', visibility: loadedImages[index] ? 'visible' : 'hidden' }}
+                      alt={`${work.title}`}
+                      onLoad={() => handleImageLoad(index)}
+                    />
+                    {loadedImages[index] && (
+                        <div className="flex justify-between">
+                            <p>{work.title}</p>
+                            <p className='opacity-50'>{work.customer}</p>
+                        </div>
+                    )}
                 </div>
               </div>
             ))}
